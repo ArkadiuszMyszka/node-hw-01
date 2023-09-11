d1f9f97aed0a26fff643aa65e19195504c7beed5
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-09-11 19:21:37 +0200

    Step 2 done

--- contacts.js
+++ contacts.js
@@ -1,25 +1,93 @@
 const fs = require("fs");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-// console.log('test')
-/*
- * Skomentuj i zapisz wartość
- * const contactsPath = ;
- */
 
-// TODO: udokumentuj każdą funkcję
+contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
 function listContacts() {
-  // ...twój kod
+  fs.readFile(contactsPath, function (err, data) {
+    if (err) {
+      console.log(`Reading error: ${err.message}`)
+      return;
+    } else {
+      const contacts = JSON.parse(data);
+      console.log("Contacts list:");
+      console.table(contacts);
+    }
+  });
 }
 
 function getContactById(contactId) {
-  // ...twój kod
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      console.log(`Reading error: ${err.message}`)
+      return;
+    }
+    const contacts = JSON.parse(data);
+  
+    const contactById = contacts.find(({ id }) => id === contactId);
+   
+    if (!contactById) {
+     console.log(`Contact with ID: ${contactId} was not found.`);
+      return;
+    }
+    console.log("Contacts by ID:");
+    console.table([contactById]);
+  });
 }
+
 
 function removeContact(contactId) {
-  // ...twój kod
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      console.log(`Reading error: ${err.message}`)
+      return;
+    }
+    const contacts = JSON.parse(data);
+  
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+   
+    if (contactIndex === -1) {
+      console.log(`Contact with ID: ${contactId} was not found.`);
+      return;
+    }
+    const removedContacts = contacts.splice(contactIndex, 1);
+     fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
+      if (err) {
+        console.log(`Reading error: ${err.message}`);
+        return;
+      }
+      console.log("Removed contact:");
+      console.table(removedContacts);
+    });
+  });
 }
 
+
 function addContact(name, email, phone) {
-  // ...twój kod
+  fs.readFile(contactsPath, (err, data) => {
+    if (err) {
+      console.log(`Reading error: ${err.message}`)
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const newContact = { id: nanoid(), name, email, phone };
+    contacts.push(newContact);
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
+      if (err) {
+        console.log(`Reading error: ${err.message}`);
+        return;
+      }
+      console.log("Contact added successfully");
+      console.table(newContact);
+    });
+  });
 }
+
+module.export = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
